57fb0e0a0b9ef795942635faebb0ef642bf35a7a
5nik1 <5nik1@mail.ru>
2026-03-30 22:55:07 +0300

    Корректировка верстки pixel-perfect

--- index.php
+++ index.php
@@ -4,9 +4,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <link rel="stylesheet" href="/styleme.css">
     <meta charset="utf-8">
+    <link rel="icon" type="image/png" sizes="32x32" href="/upload/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="/upload/favicon-48x48.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/upload/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/upload/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/upload/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="180x180" href="/upload/favicon-180x180.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/upload/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="167x167" href="/upload/apple-touch-icon-167x167.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/upload/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/upload/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/upload/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/upload/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/upload/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/upload/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/upload/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/upload/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="/upload/apple-touch-icon-57x57.png">
     <title>avilon-tank</title>
-    <style>
-    </style>
 </head>
 <body>
     <header class="header">
